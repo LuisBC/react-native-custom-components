bc05935b527c70e20c50bc4668b8f510337e1c36
Luis Barriga Carrasco <luisbarrigacarrasco@gmail.com>
2020-07-29 22:25:14 +0200

    First Commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+  },
+});
+
+const App = () => {
+  console.log('HOLA LUIS')
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
